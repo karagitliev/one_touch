13f4ca23a711d9b918fd879346d3a12e729d1282
Hristo Karagitliev <karagitliev@Hristos-iMac.local>
2019-05-14 21:50:44 +0300

    added: general user info user payment instruments user payment instruments balance

--- user_info.py
+++ user_info.py
@@ -1,48 +1,75 @@
 from pprint import pprint
 import random
 import requests
-import logging
+
+import urllib.parse
 
 SESSION = random.randint(10000, 99999)
 TIMEOUT = 30
 AUTH_TIMEOUT = 900
 
-USR_INFO_1 = 'https://demo.epay.bg/xdev/api/user/info?'
-USR_INFO_2 = 'https://demo.epay.bg/xdev/api/user/info/pins?'
-USR_INFO_3 = 'https://demo.epay.bg/xdev/api/user/info/pins/balance?'
+USR_BASE_URL = 'https://demo.epay.bg/xdev/api/user/'
 
 DEVICEID = '1510691760'
 TOKEN = '78935936003423892015399874824155'
 APPID = '6609898197243081281733444125044765054179316360618564706359682755'
 
 
-def send_recv_req(url, data):
-    r = requests.get(f'{url}{data}')
+def send_recv_req(url, params):
+    query_string = urllib.parse.urlencode(params)
+    r = requests.get(url + query_string)
+    print(url + query_string)
     r.json()
 
-#    pprint(r.json())
+    # shoul check if there is answer here, or answer is correct
+
     return(r.json())
 
 
 def general_user_info():
-    get_user_info = f'{USR_INFO_1}'
-    get_user_info_params = f'APPID={APPID}&DEVICEID={DEVICEID}&TOKEN={TOKEN}'
-    user_info = send_recv_req(get_user_info, get_user_info_params)
+    params = {
+        'APPID': APPID,
+        'DEVICEID': DEVICEID,
+        'TOKEN': TOKEN,
+    }
+    url = USR_BASE_URL + 'info?'
+    user_info = send_recv_req(url, params)
 
+    print('\n### General user info ###')
     pprint(user_info['userinfo'])
-#    pprint(user_info)
 
 
 general_user_info()
 
 
-def get_user_pay_instrum():
-    get_user_pins = f'{USR_INFO_2}'
-    get_user_pins_params = f'APPID={APPID}&DEVICEID={DEVICEID}&TOKEN={TOKEN}'
-    user_info = send_recv_req(get_user_pins, get_user_pins_params)
+def get_user_pay_instrum_balance(pins):
+    print(pins)
+    params = {
+        'APPID': APPID,
+        'DEVICEID': DEVICEID,
+        'TOKEN': TOKEN,
+        'PINS': pins,
+    }
+    url = USR_BASE_URL + 'info/pins/balance?'
+    user_info_pins_balance = send_recv_req(url, params)
 
-#    pprint(user_info['userinfo'])
-    pprint(user_info)
+    print('\n### User payment instruments balance ###')
+    pprint(user_info_pins_balance)
+
+
+def get_user_pay_instrum():
+    params = {
+        'APPID': APPID,
+        'DEVICEID': DEVICEID,
+        'TOKEN': TOKEN,
+    }
+    url = USR_BASE_URL + 'info/pins?'
+    user_info_pins = send_recv_req(url, params)
+
+    print('\n### User payment instruments info ###')
+    pprint(user_info_pins)
+
+    pins_info = get_user_pay_instrum_balance(user_info_pins['payment_instruments'][0]['ID'])
 
 
 get_user_pay_instrum()
