1355a2e3e357f232bf05d9e6c89bce353668145a
Hristo Karagitliev <karagitliev@Hristos-iMac.local>
2019-05-15 23:53:38 +0300

    d

--- onetouch_send_recv.py
+++ onetouch_send_recv.py
@@ -1,11 +1,26 @@
+import sys
 import requests
 import urllib.parse
+import onetouch_app as app
+
+from time import sleep
 
 
 def send_recv(url, params, req_type):
     query_string = urllib.parse.urlencode(params)
-    r = requests.get(url + query_string)
-    print(url + query_string)
-    r.json()
+    req = requests.get(url + query_string)
 
-    return(r.json())
+    if req.status_code == requests.codes.ok:
+        return(req.json())
+    else:
+        failcount = 0
+        while req.status_code != requests.codes.ok:
+            sleep(3)
+            failcount += 3
+            req = requests.get(url + query_string)
+            if failcount > app.AUTH_TIMEOUT:
+                pass
+        if req.status_code == requests.codes.ok:
+            return(req.json())
+        else:
+            sys.exit('AUTHORISATION TIMEOUT')
